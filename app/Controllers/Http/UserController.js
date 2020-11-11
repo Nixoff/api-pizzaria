984c6cc2f1df4648d870c58249fec961b67c708e
Gabriel <gabadev48@protonmail.com>
2020-11-11 17:12:34 -0300

    validator and database configured

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const User = use('App/Models/User');
+const { validateAll } = use('Validator');
+
+class UserController {
+  async create({ request, response }) {
+    try {
+
+
+
+      const validation = await validateAll(request.all(), {
+        username: 'required|min:5|unique:users',
+        email: 'required|email|unique:users',
+        password: 'required|min:6'
+      })
+
+      if(validation.fails()){
+        return response.status(401).send({message: validation.messages()})
+      }
+
+      const data = request.only(["username", "email", "password"])
+
+      const user = await User.create(data)
+
+      return user
+    } catch (err) {
+      return response.status(500).send({ error: `Erro: ${err.message}`})
+    }
+
+  }
+}
+
+module.exports = UserController
